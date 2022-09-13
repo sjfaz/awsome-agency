8a7e5c29eda826ac0548d41cfce689b11599c4f1
Shaun Farrell <>
2022-09-14 00:13:08 +0100

    chore: fix footer text

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -97,7 +97,7 @@ function App() {
       </div>
       <footer className="footer footer-center p-4 bg-base-300 text-base-content">
         <div>
-          <p>Copyright © 2022 - All right reserved by Awsome Agency Ltd</p>
+          <p>Copyright © 2022 - All right reserved by AWSome Agency Ltd</p>
         </div>
       </footer>
     </div>
