2a28e37e1ae6a442c989b4a3ec3570bc187255d6
Shaun Farrell <>
2022-09-23 20:20:33 +0100

    chore: fix so only fires with valid email

--- web/src/components/OrderHistory.tsx
+++ web/src/components/OrderHistory.tsx
@@ -9,15 +9,17 @@ interface OrderHistoryProps {
 export const OrderHistory: React.FC<OrderHistoryProps> = (
   props: OrderHistoryProps
 ) => {
-  const getOrders = trpc.useQuery(["getOrders", props.email]);
   const validEmail = validateEmail(props.email);
+  const getOrders = validEmail
+    ? trpc.useQuery(["getOrders", props.email])
+    : undefined;
 
   return (
     <div className="">
       <div className="pt-5 text-center">
-        {validEmail || getOrders.isLoading
-          ? "Enter valid email..."
-          : getOrders.data?.Items?.map((order, i) => (
+        {getOrders?.isLoading
+          ? "Loading..."
+          : getOrders?.data?.Items?.map((order, i) => (
               <div key={order.createdDate} className="mt-3">
                 <p className="text-l font-bold">
                   {`${order.createdDate} - ${order.email} - ${order.orderType}`}
